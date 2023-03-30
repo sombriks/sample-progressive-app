6d9ba831f3723da55ba899c3269cdc872fb185dd
sombriks <sombriks@gmail.com>
2023-03-30 14:47:01 -0300

    initial import

---
+++ src/test/java/sample/progressive/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.progressive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
